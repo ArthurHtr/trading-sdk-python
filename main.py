0b79867827ba8108f3356b91dda36babb3491983
Arthur Hottier <arthur.ho2tier@gmail.com>
2025-11-25 15:35:36 +0100

    changement clé, gitginore et local_config

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# main.py
+ 
+from config_local import BASE_URL, API_KEY
 from sdk_client import TradingClient
 
+
 client = TradingClient(
-    base_url="http://localhost:3000",
-    api_key="keyRDhQAMthOoFYKqGoBrtJEvuPRCrcXMLeHMjjNdsSYczrKADbLKRaiaRuhwLpiRjP"
+    base_url=BASE_URL,
+    api_key=API_KEY
 )
+
 print(client.ping())
